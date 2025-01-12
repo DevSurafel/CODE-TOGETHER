5930998b4439d7eee8dcc436660871ba6389b39b
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-01-12 19:30:41 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Ensure this is imported so the styles are applied globally
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
