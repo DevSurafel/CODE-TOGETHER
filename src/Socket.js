3da06d7ced14cbc434730bb8a8e0e8078775ef97
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-01-13 01:21:33 +0530

    Update Socket.js

--- src/Socket.js
+++ src/Socket.js
@@ -2,29 +2,28 @@ import { io } from "socket.io-client";
 
 export const initSocket = async () => {
   const options = {
-    'force new connection': true,
-    reconnectionAttempts: 'Infinity',
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
     timeout: 10000,
-    transports: ['websocket'],
+    transports: ["websocket"],
   };
 
   try {
-    // Use process.env.REACT_APP_BACKEND_URL for WebSocket connection
-    const socket = io(process.env.REACT_APP_BACKEND_URL, options);
+    const socket = io(`${process.env.REACT_APP_BACKEND_URL}/.netlify/functions/server`, options);
 
     return await new Promise((resolve, reject) => {
       socket.on("connect", () => {
         console.log("WebSocket connected:", socket.id);
-        resolve(socket); // Connection is successful
+        resolve(socket);
       });
 
       socket.on("connect_error", (err) => {
         console.error("Connection error:", err.message);
-        reject(new Error("Socket connection failed. Please try again!")); // Handle connection error
+        reject(new Error("Socket connection failed. Please try again!"));
       });
     });
   } catch (error) {
     console.error("Error initializing WebSocket:", error);
-    throw error; // Re-throw for the caller
+    throw error;
   }
 };
