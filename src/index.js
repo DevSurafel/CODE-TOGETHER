70a4be51e3a233310c510600e4a8174f5d828d79
Surafel Diriba <119774330+DevSurafel@users.noreply.github.com>
2025-03-29 13:50:59 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // Ensure this is imported so the styles are applied globally
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
